b16380e455b5df0acb4741ad722e2bc9c4093259
Kanzen <kanzenproctcme@proton.me>
2023-08-08 00:04:42 -0400

    Removed goto to use do while instead

--- src/array.c
+++ src/array.c
@@ -1,76 +1,67 @@
 #include <ncurses.h>
-#include <time.h>
-#include <stdlib.h>
 #include <array.h>
 
-void arrSize(arrTyp *ptr) {
+void arraySize(Array *ptr) {
   initscr();
-  srand(time(NULL));
-  wrongInput:
-  clear();
-
-  printw("- Random Character Generator -\n"
-  "Enter the length desired [1, 2, 3- ...]: ");
-  refresh();
-  scanw("%i", &(ptr->length));
-
-  if((ptr->length) <= 0) {
-    printw("Unsigned integers only!\n");
-    refresh();
-    napms(1000);
+  do {
     clear();
-    goto wrongInput;
-  }
+    printw("- Random Character Generator -\n"
+    "Enter the length desired [unsigned integers only]: ");
+    refresh();
+    scanw("%i", &(ptr->length));
+
+    if(ptr->length > 0) break;
+    printw("Value unknown.\n");
+    refresh();
+    napms(500);
+  } while(ptr->length < 1);
 }
 
-void arrType(arrTyp *ptr) {
-  wrongInput:
-  clear();
-  printw("Select the option to generate [1, 2... or 1234, 13...]:\n"
-  "1 - lower [a-z]\n"
-  "2 - upper [A-Z]\n"
-  "3 - numbers [0-9]\n"
-  "4 - symbols ['!@#$...]\n");
-  refresh();
-  scanw("%i", &(ptr->type));
-
-  if((ptr->type) <= 0) {
-    printw("Unsigned integers only!\n");
-    refresh();
-    napms(1000);
+void arrayType(Array *ptr) {
+  do {
     clear();
-    goto wrongInput;
-  }
+    printw("Select the option specified below to generate [ascending order]:\n"
+    "1 - lower [a-z]\n"
+    "2 - upper [A-Z]\n"
+    "3 - numbers [0-9]\n"
+    "4 - symbols ['!@#$...]\n");
+    refresh();
+    scanw("%hi", &(ptr->type));
+
+    if(ptr->type > 0) break;
+    printw("Option unknown!\n");
+    refresh();
+    napms(500);
+  } while(ptr->type < 1);
 }
 
-void arrOutput(arrTyp *ptr) {
+void arrayOutput(Array *ptr) {
   clear();
   printw("Selected characters: %s\n"
   "%i characters has been generated: %s\n"
   "Size in bits: %i\n\n"
   "Press \"s\" to start over\n"
   "Press \"r\" to recreate\n"
-  "Press any other key to exit...", ptr->genArr, ptr->counter, ptr->genChar, ptr->counter * 8);
+  "Press any other key to exit...",
+  ptr->genArray, ptr->count, ptr->genChar, ptr->count * 8);
   refresh();
+  arrayFree(ptr);
   ptr->choice = getch();
-  
-  free(ptr->genChar);
-  free(ptr->genArr);
-  arrRepeat(ptr);
+  arrayRepeat(ptr);
   endwin();
 }
 
-void arrRepeat(arrTyp *ptr) {
+void arrayRepeat(Array *ptr) {
   if(ptr->choice == 's') {
     ptr->length = 0;
     ptr->type = 0;
-    arrSize(ptr);
-    arrType(ptr);  
-    arrCases(ptr);
-    arrOutput(ptr);
+    arraySize(ptr);
+    arrayType(ptr);  
+    arrayCases(ptr);
+    arrayOutput(ptr);
   }
-  if(ptr->choice == 'r') {
-    arrCases(ptr);
-    arrOutput(ptr);
+  else if(ptr->choice == 'r') {
+    arrayCases(ptr);
+    arrayOutput(ptr);
   }
 }
