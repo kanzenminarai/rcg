dc44ca7c922b04b7b4d13c7ee808a2a207c66405
Amavect <amavect@gmail.com>
2023-08-12 22:15:41 -0500

    src/main.c: comment main function calls

--- src/main.c
+++ src/main.c
@@ -4,10 +4,9 @@ int main() {
   // setting ar as a variable from "Array" type
   Array ar;
   
-  // initializing the software
-  arraySize(&ar);
-  arrayType(&ar);
-  arrayCases(&ar);
-  arrayOutput(&ar);
+  arraySize(&ar); // request the number of characters,
+  arrayType(&ar); // request the types of characters to include,
+  arrayCases(&ar); // evaluating the request,
+  arrayOutput(&ar); // and printing the result.
   return 0;
 }
