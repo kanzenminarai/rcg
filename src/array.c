ff4c55656b2a62e9c387db50410f4fc545c0615a
Kanzen <kanzenproctcme@proton.me>
2023-08-14 21:47:20 -0400

    Renamed variables, changed some positions and shrinked the code

--- src/array.c
+++ src/array.c
@@ -3,10 +3,9 @@
 #include <array.h>
 
 void arraySize(Array *ptr) {
-  initscr();
-
-  // get the size of the array
   ptr->length = 0;
+  
+  // get the size of the array
   for(;;) {
     clear();
     printw("- Random Character Generator -\n"
@@ -15,29 +14,29 @@ void arraySize(Array *ptr) {
     scanw("%u", &(ptr->length));
 
     if(ptr->length > 0) break; // breaks the loop in case of a known value
-    printw("Value unknown.\n");
+    printw("Value unknown!\n");
     refresh();
     napms(500);
   }
 }
 
 void arrayType(Array *ptr) {
-  char in[32];
+  char input[4];
   size_t i;
+  ptr->type = 0;
   
   // get the type of the characters
-  ptr->type = 0;
-  for(;;) {
+  for(;;) { 
     clear();
-    printw("Select the option specified below to generate [ascending order]:\n"
+    printw("Select the option specified below to generate [any order]:\n"
     "1 - lower [a-z]\n"
     "2 - upper [A-Z]\n"
     "3 - numbers [0-9]\n"
     "4 - symbols ['!@#$...]\n");
     refresh();
-    getnstr(in, sizeof(in));
-    for(i = 0; in[i]; i++){
-      switch(in[i]){
+    getnstr(input, sizeof(input));
+    for(i = 0; input[i]; i++){
+      switch(input[i]){
         case '1':
           ptr->type |= LOWER;
           break;
@@ -53,13 +52,17 @@ void arrayType(Array *ptr) {
         case '4':
           ptr->type |= SYMBOL;
           break;
+        
+        default:
+          if(input[i] != '\04') {
+            printw("Unknown option [%c]!\n", input[i]);
+            refresh();
+            napms(800);
+          }
+          break;
       }
-    }
-
-    if(ptr->type > 0) break; // breaks the loop in case of a known value
-    printw("Option unknown!\n");
-    refresh();
-    napms(500);
+    }  
+    if(ptr->type > 0) break; // breaks the for loop
   }
 }
 
