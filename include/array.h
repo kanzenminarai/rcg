288e7be695765c3255c78496e5b96cd67cf1e5ac
Kanzen <kanzenproctcme@proton.me>
2023-11-07 18:14:32 -0400

    Added new files, moved some functions and other things

--- include/array.h
+++ include/array.h
@@ -1,31 +1,12 @@
 #ifndef ARRAY_H
 #define ARRAY_H
 
-#define LOWER 1<<0
-#define UPPER 1<<1
-#define NUMBER 1<<2
-#define SYMBOL 1<<3
-#define LETTERS 26
-#define NUMBERS 10
-#define SYMBOLS 30
+#include <var.h>
 
-typedef struct {
-  unsigned int length;
-  unsigned short int type;
-  char *genChar;
-  char *genArray;
-} Array;
+// insert the values of each char range from the given input
+void charOptions(char const *option, Array *ptr);
 
 // creates a array and get the selected range of characters 
 void arrayCreation(Array *ptr);
-
-// get the size of the array
-void arraySize(Array *ptr);
-
-// get the elements for the array 
-void arrayType(Array *ptr);
-
-// prints the output of the generated characters
-void arrayOutput(Array *ptr);
 
 #endif // ARRAY_H
--- include/rand.h
+++ include/rand.h
@@ -1,5 +1,7 @@
 #ifndef RAND_H
 #define RAND_H
+
+// get random bytes from /dev/urandom through getrandom();
 unsigned int getNextRandom(unsigned int max);
 
 #endif
