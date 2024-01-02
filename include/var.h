7ba96083f4438051304179af50d6508d4ef91566
Kanzen Minarai <kanzenminarai@proton.me>
2024-01-02 00:00:14 -0400

    changed some variables types, added stdbool.h and stdint.h

--- include/var.h
+++ include/var.h
@@ -1,12 +1,17 @@
 #ifndef VAR_H
 #define VAR_H
 
-typedef struct {
-  char scrStart;
-  unsigned int length;
-  unsigned short int type;
-  char *genChar;
-  char *genArray;
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct Array {
+  bool wrongInput;
+  uint8_t arrayRange;
+  uint16_t characterLength;
+  uint16_t characterType;
+  char *generatedChararacters;
+  char *generatedArray;
+  uint8_t argumentCount : 3;
 } Array;
 
 #endif // VAR_H
