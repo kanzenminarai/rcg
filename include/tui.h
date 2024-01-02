3aeef4a606111477a30eade57f01ece9bc99a0ac
Kanzen Minarai <kanzenminarai@proton.me>
2024-01-01 23:59:57 -0400

    changed some functions names, added var.h

--- include/tui.h
+++ include/tui.h
@@ -3,13 +3,9 @@
 
 #include <var.h>
 
-// get the size of the array
-void arraySize(Array *ptr);
-
-// get the elements for the array 
-void arrayType(Array *ptr);
-
-// prints the output of the generated characters
-void arrayOutput(Array *ptr);
+void getArrayLength(Array *ptr);
+void getArrayType(Array *ptr);
+void showGeneratedCharacters(Array *ptr);
+void tuiMain(Array *ptr);
 
 #endif // TUI_H
